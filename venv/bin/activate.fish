a2a2e1e9ed2f97f1b27442090348880fa58ecad8
YeounJun Park <pyj4104@hotmail.com>
2015-07-15 20:45:36 -0400

    Installed other dependencies.

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/yeounjunpark/Desktop/CurrentWork/KWDoughnutCafe/Doughnut"
+set -gx VIRTUAL_ENV "/Users/yeounjunpark/Desktop/CurrentWork/KWDoughnutCafe/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
